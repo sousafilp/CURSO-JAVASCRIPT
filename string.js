72810ba0bcd346cc1d296b642a78f02fd6d2dad2
sousafilp <sousaafilp@gmail.com>
2024-03-08 15:06:34 -0300

    aula referente a variáveis e tipos de dados

---
+++ string.js
@@ -0,0 +1,40 @@
+// Strings
+
+// const firstName = "Filipe";
+// const lastName = "Sousa";
+
+// const names = "Filipe, João, Julia";
+
+// const list = names.split(",");
+
+// console.log(list);
+
+// console.log(`meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}!`);
+
+// NUMBERS
+
+// const number = 5;
+
+// console.log(number);
+
+// console.log(typeof number.toString());
+
+// Boolean
+
+// true ou false
+
+// console.log(2 == 2);
+
+// Null & Undefined
+
+// const x = null; // é como se fosse vazia
+
+// const y = undefined; // é como se não fosse definida ainda
+
+// Lista
+
+// const list = [1, 2, 3, 4, 5];
+
+// Objetos
+
+// const object = { name: "Felipe" }; // Pode guardar propriedades e valores
